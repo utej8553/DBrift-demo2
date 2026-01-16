15d23a2db798cc8543faf3bfd09bb61551e16cb0
Desky <91786744+utej8553@users.noreply.github.com>
2026-01-16 20:21:38 +0530

    Update DatabaseService.java

--- backend/src/main/java/org/utej/backend/DatabaseService.java
+++ backend/src/main/java/org/utej/backend/DatabaseService.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 @Service
 public class DatabaseService {
+    private final DatabaseRepository databaseRepository;
 
     private final PortService portService;
 
@@ -125,5 +126,6 @@ public DatabaseService(DatabaseRepository databaseRepository,
         }
     }
 }
+
 
 
